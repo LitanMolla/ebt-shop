e33738d7583846c6cf9fcc45b2caea6456e17f6d
LitanMolla <litanmolla9@gmail.com>
2025-11-06 03:47:25 +0600

    fav icon and title added and key

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
                 <div className="container">
                     <h2 className='text-3xl text-center pb-5 font-semibold'>Latest <span className='text-cyan-400'>Products</span></h2>
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 xl:grid-cols-4">
-                        {products?.slice(0,8).map(product=><ProductCard product={product} />)}
+                        {products?.slice(0,8).map(product=><ProductCard key={product.id} product={product} />)}
                     </div>
                 </div>
             </div>
